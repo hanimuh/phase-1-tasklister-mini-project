c13cbfff41e19df7418413f3c8b4c3e18ac5d1b2
Hani <md.hani@hotmail.com>
2022-05-14 10:11:14 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
+// your code here
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form= document.querySelector('form');
+  form.addEventListener('submit',function(e){
+    e.preventDefault();
+    let inpt= document.getElementById('new-task-description').value;
+    handle(inpt);
+  } )
 });
+function handle(elem){
+  let li= document.createElement('li');
+  li.setAttribute('id', 'li')
+  li.textContent= elem;
+  document.getElementById('tasks').appendChild(li);
+  let button= document.createElement('button');
+  button.textContent='X'
+  li.appendChild(button);
+  button.addEventListener('click',(btn)=>btn.target.parentNode.remove())
+  const select= document.createElement('select');
+  select.setAttribute('id','sel');
+  document.querySelector('form').appendChild(select);
+  const opt0= document.createElement('option');
+  opt0.innerText=''
+  const opt1= document.createElement('option');
+  opt1.innerText='high priority'
+  const opt2= document.createElement('option');
+  opt2.innerText='medium priority'
+  const opt3= document.createElement('option');
+  opt3.innerText='low priority'
+  select.appendChild(opt0)
+  select.appendChild(opt1)
+  select.appendChild(opt2)
+  select.appendChild(opt3)
+  select.addEventListener('change',function(){
+    document.getElementById('li').style.color= 'red'
+  })
+}
+
+// document.getElementById('li').style.color= 'red'
+// console.log(document.getElementById('new-task-description').value)
